24401ff474d38994ced88f4605088027f152ff71
t.durcak@astoundcommerce.com <t.durcak@astoundcommerce.com>
2020-04-06 11:55:55 +0200

    metadata init

---
+++ cartridges/int_algolia/cartridge/scripts/algolia/lib/algoliaData.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var currentSite = require('dw/system/Site').getCurrent();
+
+/*
+ * Function for getting preferences for Sift
+ *
+ *   id                     | Description                                       |type of preference
+ *   -----------------------------------------------------------------------------------------------
+ *   ApplicationID          | Identifies the application for this site          | String
+ *   CustomFields           | Any additional attributes of Product Object       | Set-of-string
+ *   Enable                 | Enable/disable all Algolia                        | Boolean
+ *   HostBase               | Host for read operations                          | String
+ *   InStockThreshold       | Stock Threshold                                   | Double
+ *   LastCategorySyncDate   | Timestamp of the last Category index sync job run | Datetime
+ *   LastProductSyncDate    | Timestamp of the last product index sync job run  | Datetime
+ *   SearchApiKeyLimit      | Authorization key for Algolia                     | String
+ */   -----------------------------------------------------------------------------------------------
+ //   Example:
+ //    var siftPref = require('*/cartridge/scripts/algolia/lib/algoliaData');
+ //    siftPref('Enable');
+ //   -----------------------------------------------------------------------------------------------
+ 
+
+/**
+ * @description Getting preferences for Algolia
+ * @param {string} id - name of preference
+ * @returns {*} - value of preference
+ */
+function getPreference(id) {
+	var value = currentSite.getCustomPreferenceValue('Algolia_' + id);
+	if (value !== null) {
+		return value;
+	}
+    return false;
+}
+
+module.exports = getPreference;
