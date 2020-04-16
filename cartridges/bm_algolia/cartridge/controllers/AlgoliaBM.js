e725ce5399aefac298674ec7b39d3c987ebac672
Rudolf Fentsyk <r.fentsyk@astoundcommerce.com>
2020-04-16 10:15:03 +0300

    Merge bm-extension

---
+++ cartridges/bm_algolia/cartridge/controllers/AlgoliaBM.js
@@ -0,0 +1,78 @@
+'use strict';
+
+/* Script Modules */
+var ISML = require('dw/template/ISML');
+var URLUtils = require('dw/web/URLUtils');
+var Logger = require('dw/system/Logger');
+
+var algoliaData = require('*/cartridge/scripts/algolia/lib/algoliaData');
+
+/**
+ * @description Render default template
+ * @returns {void} - ISML.renderTemplate
+ */
+function renderIndex() {
+    var pdictValues = {
+        reindexUrl: URLUtils.https('AlgoliaBM-HandleReindex'),
+        setttingsUpdateUrl: URLUtils.https('AlgoliaBM-HandleSettings'),
+        algoliaData: algoliaData
+    }
+
+    ISML.renderTemplate('algoliabm/dashboard/index', pdictValues);
+}
+
+/**
+ * @description Main pipelet
+ * @returns {void} - renderIndex
+ */
+function start() {
+    renderIndex();
+}
+
+/**
+ * @description Handle form for reindex
+ * @returns {void} - renderIndex
+ */
+function handleReindex() {
+    var params = request.httpParameterMap;
+
+    if (params.reindexAll.submitted) {
+    // TODO: reindex
+    } else if (params.reindexProducts.submitted) {
+    // TODO: reindex
+    } else if (params.reindexCategories.submitted) {
+        // TODO: reindex
+    }
+    renderIndex();
+}
+
+/**
+ * @description Handle form for settings
+ * @returns {void} - renderIndex
+ */
+function handleSettings() {
+    var params = request.httpParameterMap;
+    try {
+        var algoliaEnable = ('Enable' in params) && (params.Enable.submitted == true) ? true : false;
+        algoliaData.setPreference("Enable", algoliaEnable);
+        algoliaData.setPreference("ApplicationID", params.ApplicationID.value);
+        algoliaData.setSetOfStrings("CustomFields", params.CustomFields.value);
+        algoliaData.setPreference("HostBase", params.HostBase.value);
+        algoliaData.setPreference("InStockThreshold", params.InStockThreshold.value * 1);
+        algoliaData.setPreference("LastCategorySyncDate", new Date(params.LastCategorySyncDate.value));
+        algoliaData.setPreference("LastProductSyncDate", new Date(params.LastProductSyncDate.value));
+        algoliaData.setPreference("SearchApiKey", params.SearchApiKey.value);
+    } catch(error) {
+    	Logger.error(error);
+    }
+
+    renderIndex();
+}
+
+
+exports.Start = start;
+exports.Start.public = true;
+exports.HandleReindex = handleReindex;
+exports.HandleReindex.public = true;
+exports.HandleSettings = handleSettings;
+exports.HandleSettings.public = true;
---
+++ cartridges/int_algolia/cartridge/scripts/algolia/lib/algoliaData.js
@@ -0,0 +1,77 @@
+'use strict';
+
+var currentSite = require('dw/system/Site').getCurrent();
+var Transaction = require('dw/system/Transaction');
+
+/*
+ * Function for getting preferences for Algolia
+ *
+ *   id                     | Description                                       |type of preference
+ *   -----------------------------------------------------------------------------------------------
+ *   ApplicationID          | Identifies the application for this site          | String
+ *   CustomFields           | Any additional attributes of Product Object       | Set-of-string
+ *   Enable                 | Enable/disable all Algolia                        | Boolean
+ *   HostBase               | Host for read operations                          | String
+ *   InStockThreshold       | Stock Threshold                                   | Double
+ *   LastCategorySyncDate   | Timestamp of the last Category index sync job run | Datetime
+ *   LastProductSyncDate    | Timestamp of the last product index sync job run  | Datetime
+ *   SearchApiKey           | Authorization key for Algolia                     | String
+   -----------------------------------------------------------------------------------------------
+ */
+ //   Example:
+ //    var algoliaData = require('*/cartridge/scripts/algolia/lib/algoliaData');
+ //    algoliaData.getPreference('Enable');
+ //    algoliaData.setPreference('Enable', true);
+ //   -----------------------------------------------------------------------------------------------
+
+/**
+ * @description Getting preference for Algolia
+ * @param {string} id - name of preference
+ * @returns {*} - value of preference
+ */
+function getPreference(id) {
+    return currentSite.getCustomPreferenceValue('Algolia_' + id);
+}
+
+ /**
+  * @description Set preference for Algolia
+  * @param {string} id - name of preference
+  * @param {string} value - value to save
+  * @returns {void}
+  */
+function setPreference(id, value) {
+    Transaction.wrap(function(){
+        currentSite.setCustomPreferenceValue('Algolia_' + id, value);
+    });
+}
+
+/**
+ * @description Getting preference (as set of strings) for Algolia
+ * @param {string} id - name of preference
+ * @returns {array} - value of preference
+ */
+function getSetOfStrings(id) {
+    var values = currentSite.getCustomPreferenceValue('Algolia_' + id);
+    return values.length ? values.join() : ', ';
+}
+
+/**
+ * @description Set preference (as set of strings) for Algolia
+ * @param {string} id - name of preference
+ * @param {string} value - value to save
+ * @returns {void}
+ */
+function setSetOfStrings(id, value) {
+    Transaction.wrap(function(){
+        var toSave = value.replace(/ /g, '');   // remove white spaces 
+        var toSave = toSave.split(',');         // create array
+        currentSite.setCustomPreferenceValue('Algolia_' + id, toSave);
+    });
+}
+
+module.exports = {
+    getPreference: getPreference,
+    setPreference: setPreference,
+    getSetOfStrings: getSetOfStrings,
+    setSetOfStrings: setSetOfStrings
+}
