db82a926a5850eb726168c7dd9cb5206e94ae792
y_chechuha <y.chechuha@astoundcommerce.com>
2020-04-23 10:32:47 +0300

    Updated and fixed bugs

--- cartridges/int_algolia/cartridge/scripts/algolia/job/sendDelta.js
+++ cartridges/int_algolia/cartridge/scripts/algolia/job/sendDelta.js
@@ -5,44 +5,53 @@ var QUOTA_API_JS_JSON_STRING_LENGTH = 600000; // The maximum allowed length of a
 var MAX_FAILED_CHUNKS = 3;
 
 var logger = require('dw/system/Logger').getLogger('algolia', 'Algolia');
-var failedChunks = [];
 
 /**
  * Send array of objects to Algolia API
  * @param {Array} entriesArray - array of objects for send to Algolia
- * @returns {null} - successful to send
+ * @returns {boolean} - successful to send
  */
 function sendChunk(entriesArray) {
     var algoliaApi = require('*/cartridge/scripts/algoliaApi');
     var result = algoliaApi.sendDelta(entriesArray);
-    if (result.error) {
-        // save chunks for retry
-        failedChunks = failedChunks.push(entriesArray);
-        if (failedChunks.length > MAX_FAILED_CHUNKS) {
-            throw new Error('Too many failed chunks. Service might be down. Aborting the job.');
-        }
-    }
-    return null;
+    return !result.error;
 }
 
 /**
  * Resending unsent objects to Algolia API
- * @param {Array} entriesArray - array of objects for send to Algolia
+ * @param {Array} failedChunks - array of objects for send to Algolia
+ * @returns {boolean} - successful
  */
-function sendFailedChunks() {
-    failedChunks.forEach(function (elements) {
-        sendChunk(elements);
-    });
+function sendFailedChunks(failedChunks) {
+    if (failedChunks.length == 0) {
+        return true;
+    }
+
+    var chunkLength = Math.floor(failedChunks.length / MAX_FAILED_CHUNKS) + 1;
+
+    for(var startIndex = 0; startIndex < failedChunks.length; startIndex += chunkLength ) {
+        var elements = failedChunks.slice(startIndex, startIndex + chunkLength);
+        if (!sendChunk(elements)) {
+            return false;
+        };
+    };
+
+    return true;
 }
 
-module.exports.execute = function (parameters, stepExecution) {
+module.exports.execute = function (parameters) {
     var productDeltaIterator = require('*/cartridge/scripts/algolia/helper/productDeltaIterator');
     var entries = [];
     var maxNumberOfEntries;
+    var failedChunks = [];
+    var countFailedShunks = 0;
+
     var deltaList = productDeltaIterator.create();
 
     if (deltaList.getSize() === 0) {
         logger.info('Delta is empty, no syncronization is needed');
+        deltaList.close();
+        return true;
     }
 
     // check if merchant set his preferred number
@@ -50,34 +59,31 @@ module.exports.execute = function (parameters, stepExecution) {
         maxNumberOfEntries = parameters.maxNumberOfEntries;
     } else {
         // calculate it
-
-        var firstEntry = deltaList.next(); // get first product record
-        entries.push(firstEntry); // store it to be sent
-
-        // calculate size of one object
-        // @TODO: clculate average product length when creating a delta file and add it to the delta xml header.
-        var sampleEntryLength = JSON.stringify(firstEntry).length;
-        maxNumberOfEntries = Math.floor(QUOTA_API_JS_JSON_STRING_LENGTH / sampleEntryLength); // number of objects to fit the quota
+        maxNumberOfEntries = Math.floor(QUOTA_API_JS_JSON_STRING_LENGTH / deltaList.getRecordSize()); // number of objects to fit the quota
         maxNumberOfEntries -= Math.floor(maxNumberOfEntries / 5); // reduce by 20%
 
         logger.debug('Calculated maximum product in a chunk (maxNumberOfEntries) : {0}', maxNumberOfEntries);
     }
 
     while (deltaList.hasNext()) {
-        if (entries.length >= maxNumberOfEntries) {
-            sendChunk(entries); // send the chunks
-            entries.length = 0; // crear the array
-        } else {
-            entries.push(deltaList.next());
+        entries.push(deltaList.next());
+
+        if (entries.length >= maxNumberOfEntries || !deltaList.hasNext()) {
+            // send the chunks
+            if (!sendChunk(entries)) {
+                failedChunks = failedChunks.concat(entries);
+                if (++countFailedShunks > MAX_FAILED_CHUNKS) {
+                    throw new Error('Too many failed chunks. Service might be down. Aborting the job.');
+                    break;
+                }
+            };
+            entries.length = 0; // crear the array            
         }
     }
 
     deltaList.close();
     
-    // send the chunks left
-    sendChunk(entries);
-
     //Resending failed chunks
-    sendFailedChunks();
+    sendFailedChunks(failedChunks);
     return true;
 };
