53ddaf6a5b7fa268cdcad9166deb472f2c240bdb
y_chechuha <y.chechuha@astoundcommerce.com>
2020-05-19 15:41:49 +0300

    Moved Algolia last update Log Data to XML file

---
+++ cartridges/int_algolia/cartridge/scripts/algolia/helper/logHelper.js
@@ -0,0 +1,124 @@
+'use strict';
+
+var File = require('dw/io/File');
+var FileWriter = require('dw/io/FileWriter');
+var XMLStreamWriter = require('dw/io/XMLIndentingStreamWriter');
+var FileReader = require('dw/io/FileReader');
+var XMLStreamReader = require('dw/io/XMLStreamReader');
+
+var jobHelper = require('*/cartridge/scripts/algolia/helper/jobHelper');
+var algoliaConstants = require('*/cartridge/scripts/algolia/lib/algoliaConstants');
+
+var LOG_NODE_NAME = 'logger';
+
+/**
+ * Log Data Object
+ */
+function LogJob() {
+    this.processedDate = '---';
+    this.processedError = false;
+    this.processedErrorMessage = '';
+    this.processedRecords = 0;
+    this.processedToUpdateRecords = 0;
+    this.sendDate = '---';
+    this.sendError = false;
+    this.sendErrorMessage = '';
+    this.sendedChunk = 0;
+    this.sendedRecords = 0;
+    this.failedChunk = 0;
+    this.failedRecords = 0;
+}
+
+/**
+ * Read JS object from XML file
+ * @param {dw.io.XMLStreamReader} xmlStreamReader - XML Stream Reader
+ * @param {string} nodeName - name of node XML object
+ * @returns {Object} - JS object
+ */
+function readObject(xmlStreamReader, nodeName) {
+    var objXML = jobHelper.readXMLObjectFromStream(xmlStreamReader, nodeName);
+    var obj = jobHelper.xmlToObject(objXML);
+    return obj ? obj[nodeName] : null;
+}
+
+/**
+ * @description Get category and product log data from log file
+ * @param {string} id - name of preference [category, product]
+ * @returns {Object} - log data
+ */
+function getLogData(id) {
+    var logData = {
+        category: new LogJob(),
+        product: new LogJob()
+    };
+
+    var logFile = new File(algoliaConstants.ALGOLIA_LOG_FILE);
+
+    if (logFile.exists()) {
+        var logFileReader = null;
+        var logXMLFileReader = null;
+
+        try {
+            // Open file
+            logFileReader = new FileReader(logFile, 'UTF-8');
+            logXMLFileReader = new XMLStreamReader(logFileReader);
+            logData = readObject(logXMLFileReader, LOG_NODE_NAME);
+        } catch (error) {
+            jobHelper.logFileError(algoliaConstants.ALGOLIA_LOG_FILE, 'Error open file to read', error);
+            logXMLFileReader.close();
+            logFileReader.close();
+        }
+    }
+
+    var result = null;
+    switch (id) {
+        case 'category': result = logData.category ? logData.category : null; break;
+        case 'product': result = logData.product ? logData.product : null; break;
+        case LOG_NODE_NAME: result = logData; break;
+        default: break;
+    }
+
+    return result;
+}
+
+/**
+ * @description Save product and category log data to file
+ * @param {string} id - name of preference [category, product]
+ * @param {Object} productLog - product log Object
+ * @returns {boolean} - Log data write success
+ */
+function setLogData(id, productLog) {
+    if (empty(productLog) || empty(id)) return false;
+
+    var logData = getLogData(LOG_NODE_NAME);
+    logData[id] = productLog;
+    // Open Delta XML file to write
+    var logFile = new File(algoliaConstants.ALGOLIA_LOG_FILE);
+    var logFileWriter = null;
+    var logXmlWriter = null;
+    try {
+        logFileWriter = new FileWriter(logFile, 'UTF-8');
+        logXmlWriter = new XMLStreamWriter(logFileWriter);
+        logXmlWriter.writeStartDocument();
+    } catch (error) {
+        jobHelper.logFileError(logFile.fullPath, 'Error open Log file to write', error);
+        return false;
+    }
+
+    var logXML = new XML('<' + LOG_NODE_NAME + '></' + LOG_NODE_NAME + '>');
+    jobHelper.appendObjToXML(logXML, logData);
+    logXmlWriter.writeCharacters('\n');
+    logXmlWriter.writeRaw(logXML.toXMLString());
+    logXmlWriter.writeCharacters('\n');
+
+    // Close XML Update file
+    logXmlWriter.writeEndDocument();
+    logXmlWriter.close();
+    logFileWriter.close();
+    return true;
+}
+
+module.exports = {
+    getLogData: getLogData,
+    setLogData: setLogData
+};
