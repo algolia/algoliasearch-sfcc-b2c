970a03bf33d8c4519f837abafa2a7a7b996de222
y_chechuha <y.chechuha@astoundcommerce.com>
2020-06-01 09:06:58 +0300

    Updated product model customizer

--- cartridges/int_algolia/cartridge/scripts/algolia/customization/productModelCustomizer.js
+++ cartridges/int_algolia/cartridge/scripts/algolia/customization/productModelCustomizer.js
@@ -5,19 +5,24 @@
  * @param {Array} categories - array of catefories
  * @returns {Object} - secondary category Algolia object
  */
-function categoriesToIndex(categories) {
+function categoriesToIndexTree(categories) {
     var CATEGORIES_SEPARATOR = ' > ';
+    var CATEGORIES_LEVEL_PREFIX = 'level_';
     var result = {};
-    for (var i = 0; i < categories.length; i += 1) {
-        result['level_' + i] = {};
-        var categoryNames = categories[categories.length - i - 1].name;
-        // eslint-disable-next-line no-loop-func
-        Object.keys(categoryNames).forEach(function (locale) {
-            result['level_' + i][locale] = i
-                ? result['level_' + (i - 1)][locale] + CATEGORIES_SEPARATOR + categoryNames[locale]
-                : categoryNames[locale];
-        });
-    }
+
+    categories
+        .concat()
+        .reverse()
+        .reduce(function (previousCategory, category, index) {
+            result[CATEGORIES_LEVEL_PREFIX + index] = {};
+            Object.keys(category.name).forEach(function (locale) {
+                result[CATEGORIES_LEVEL_PREFIX + index][locale] = previousCategory
+                    ? previousCategory.name[locale] + CATEGORIES_SEPARATOR + category.name[locale]
+                    : category.name[locale];
+            });
+            return category;
+        }, null);
+
     return result;
 }
 
@@ -38,22 +43,13 @@ function customizeProductModel(productModel) {
     if (empty(result.categories)) {
         result[CATEGORY_ATTRIBUTE] = null;
     } else {
-        result.categories.forEach(function (categoryTree) {
-            var rootCategoryId = categoryTree[categoryTree.length - 1].id;
-            if (rootCategoryId !== result.primary_category_id
-                && rootCategoryId === CATEGORY_ID) {
-                result[CATEGORY_ATTRIBUTE] = categoriesToIndex(categoryTree);
-            }
-        });
-        /*
         for (var i = 0; i < result.categories.length; i += 1) {
             var rootCategoryId = result.categories[i][result.categories[i].length - 1].id;
-            if (rootCategoryId !== result.primary_category_id
-                && rootCategoryId === CATEGORY_ID) {
-                result[CATEGORY_ATTRIBUTE] = categoriesToIndex(result.categories[i]);
+            if (rootCategoryId === CATEGORY_ID) {
+                result[CATEGORY_ATTRIBUTE] = categoriesToIndexTree(result.categories[i]);
+                break;
             }
         }
-        */
     }
 }
 
