a5673d9a201a705bbf83a27c814c3fad9e9baf6b
Youcef Mammar <youcef.mammar@algolia.com>
2020-08-06 13:10:48 +0200

    fix: enable search on mobile search box

--- cartridges/int_algolia_sfra/cartridge/static/default/js/algolia/autocomplete-config.js
+++ cartridges/int_algolia_sfra/cartridge/static/default/js/algolia/autocomplete-config.js
@@ -1,78 +1,92 @@
 /* global autocomplete, Hogan  */
 
 function enableAutocomplete(config) {
-    var productSuggestionTemplate = Hogan.compile(''
-        + '<div class="text-truncate text-nowrap">'
-        + '   <img class="swatch-circle hidden-xs-down" src="{{firstImage.dis_base_link}}" />'
-        + '   <a href="{{url}}">{{&_highlightResult.name.value}}</a>'
-        + '</div>'
-    );
+	var productSuggestionTemplate = Hogan.compile(
+		'' +
+			'<div class="text-truncate text-nowrap">' +
+			'   <img class="swatch-circle hidden-xs-down" src="{{firstImage.dis_base_link}}" />' +
+			'   <a href="{{url}}">{{&_highlightResult.name.value}}</a>' +
+			'</div>'
+	);
 
-    var categorySuggestionTemplate = Hogan.compile(''
-        + '<div class="text-truncate text-nowrap">'
-        + '  <img class="swatch-circle hidden-xs-down" src="{{image}}" />'
-        + '  <a href="{{url}}">{{&_highlightResult.name.value}}</a>'
-        + '</div>'
-    );
+	var categorySuggestionTemplate = Hogan.compile(
+		'' +
+			'<div class="text-truncate text-nowrap">' +
+			'  <img class="swatch-circle hidden-xs-down" src="{{image}}" />' +
+			'  <a href="{{url}}">{{&_highlightResult.name.value}}</a>' +
+			'</div>'
+	);
 
+	const inputElements = document.querySelectorAll('#aa-search-input');
 
-    autocomplete('#aa-search-input', {
-        cssClasses: {
-            dropdownMenu: "dropdownMenu suggestions p-2"
-        }
-    }, [
-        {
-            source: autocomplete.sources.hits(config.searchClient.initIndex(config.productsIndex), {
-                hitsPerPage: 3,
-                distinct: true,
-                clickAnalytics: true
-            }),
-            displayKey: 'products',
-            name: 'products',
-            templates: {
-                header: ''
-                    + '<div class="header row justify-content-end">'
-                    + '  <div class="col-xs-12 col-sm-10">' + algoliaData.strings.products + '</div>'
-                    + '</div>',
-                suggestion: function(product) {
-                    if (typeof(product.image_groups) === "undefined"){
-                        product.firstImage = algoliaData.noImages.small;
-                    } else {
-                        var smallImageGroup = product.image_groups.find(function (imageGroup) {
-                            return imageGroup.view_type === "small"
-                        });
-                        product.firstImage = smallImageGroup.images[0];
-                    }
-                    return productSuggestionTemplate.render(product)
-                }
-            }
-        },
-        {
-            source: autocomplete.sources.hits(config.searchClient.initIndex(config.categoriesIndex), {
-                hitsPerPage: 3,
-                distinct: true,
-                clickAnalytics: true
-            }),
-            displayKey: 'categories',
-            name: 'categories',
-            templates: {
-                header: ''
-                    + '<div class="header row justify-content-end">'
-                    + '  <div class="col-xs-12 col-sm-10">' + algoliaData.strings.categories + '</div>'
-                    + '</div>',
-                suggestion: function(category) {
-                    return categorySuggestionTemplate.render(category)
-                }
-            }
-        }
-    ]);
+	inputElements.forEach(function(inputElement) {
+		autocomplete(
+			inputElement,
+			{
+				cssClasses: {
+					dropdownMenu: 'dropdownMenu suggestions p-2'
+				}
+			},
+			[
+				{
+					source: autocomplete.sources.hits(config.searchClient.initIndex(config.productsIndex), {
+						hitsPerPage: 3,
+						distinct: true,
+						clickAnalytics: true
+					}),
+					displayKey: 'products',
+					name: 'products',
+					templates: {
+						header:
+							'' +
+							'<div class="header row justify-content-end">' +
+							'  <div class="col-xs-12 col-sm-10">' +
+							algoliaData.strings.products +
+							'</div>' +
+							'</div>',
+						suggestion: function(product) {
+							if (typeof product.image_groups === 'undefined') {
+								product.firstImage = algoliaData.noImages.small;
+							} else {
+								var smallImageGroup = product.image_groups.find(function(imageGroup) {
+									return imageGroup.view_type === 'small';
+								});
+								product.firstImage = smallImageGroup.images[0];
+							}
+							return productSuggestionTemplate.render(product);
+						}
+					}
+				},
+				{
+					source: autocomplete.sources.hits(config.searchClient.initIndex(config.categoriesIndex), {
+						hitsPerPage: 3,
+						distinct: true,
+						clickAnalytics: true
+					}),
+					displayKey: 'categories',
+					name: 'categories',
+					templates: {
+						header:
+							'' +
+							'<div class="header row justify-content-end">' +
+							'  <div class="col-xs-12 col-sm-10">' +
+							algoliaData.strings.categories +
+							'</div>' +
+							'</div>',
+						suggestion: function(category) {
+							return categorySuggestionTemplate.render(category);
+						}
+					}
+				}
+			]
+		);
 
-    if (document.querySelector('#aa-search-input')) {
-        document.querySelector('#aa-search-input').addEventListener('keypress', function(event){
-            if (event.key === 'Enter') {
-                var urlParams = config.searchPageRoot.indexOf("?") > -1 ? '&q=' + event.target.value : '?q=' + event.target.value;
-                window.location.href = config.searchPageRoot + urlParams;
-            }
-        });
-    }
+		inputElement.addEventListener('keypress', function(event) {
+			if (event.key === 'Enter') {
+				var urlParams =
+					config.searchPageRoot.indexOf('?') > -1 ? '&q=' + event.target.value : '?q=' + event.target.value;
+				window.location.href = config.searchPageRoot + urlParams;
+			}
+		});
+	});
 }
