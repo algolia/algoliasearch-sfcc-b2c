505f9a844a6a152351bf3e3c0f22b26af7bf2287
t.durcak@astoundcommerce.com <t.durcak@astoundcommerce.com>
2021-07-08 15:36:03 +0200

    Updates to SFRA 6.0.0

--- cartridges/int_algolia_sfra/cartridge/controllers/Search.js
+++ cartridges/int_algolia_sfra/cartridge/controllers/Search.js
@@ -55,37 +55,50 @@ server.replace('Show', cache.applyShortPromotionSensitiveCache, consentTracking.
         }
     }
     if (!useAlgolia) { // default Search-Show
-        var ProductSearchModel = require('dw/catalog/ProductSearchModel');
         var searchHelper = require('*/cartridge/scripts/helpers/searchHelpers');
+
+        if (req.querystring.cgid) {
+            var pageLookupResult = searchHelper.getPageDesignerCategoryPage(req.querystring.cgid);
+
+            if ((pageLookupResult.page && pageLookupResult.page.hasVisibilityRules()) || pageLookupResult.invisiblePage) {
+                // the result may be different for another user, do not cache on this level
+                // the page itself is a remote include and can still be cached
+                res.cachePeriod = 0; // eslint-disable-line no-param-reassign
+            }
+
+            if (pageLookupResult.page) {
+                res.page(pageLookupResult.page.ID, {}, pageLookupResult.aspectAttributes);
+                return next();
+            }
+        }
+
         var template = 'search/searchResults';
 
-        var apiProductSearch = new ProductSearchModel();
-        var viewData = {
-            apiProductSearch: apiProductSearch
-        };
-        res.setViewData(viewData);
+        var result = searchHelper.search(req, res);
 
-        this.on('route:BeforeComplete', function (req, res) { // eslint-disable-line no-shadow
-            var result = searchHelper.search(req, res);
+        if (result.searchRedirect) {
+            res.redirect(result.searchRedirect);
+            return next();
+        }
 
-            if (result.searchRedirect) {
-                res.redirect(result.searchRedirect);
-                return;
-            }
+        if (result.category && result.categoryTemplate) {
+            template = result.categoryTemplate;
+        }
 
-            if (result.category && result.categoryTemplate) {
-                template = result.categoryTemplate;
-            }
+        var redirectGridUrl = searchHelper.backButtonDetection(req.session.clickStream);
+        if (redirectGridUrl) {
+            res.redirect(redirectGridUrl);
+        }
 
-            res.render(template, {
-                productSearch: result.productSearch,
-                maxSlots: result.maxSlots,
-                reportingURLs: result.reportingURLs,
-                refineurl: result.refineurl,
-                category: result.category ? result.category : null,
-                canonicalUrl: result.canonicalUrl,
-                schemaData: result.schemaData
-            });
+        res.render(template, {
+            productSearch: result.productSearch,
+            maxSlots: result.maxSlots,
+            reportingURLs: result.reportingURLs,
+            refineurl: result.refineurl,
+            category: result.category ? result.category : null,
+            canonicalUrl: result.canonicalUrl,
+            schemaData: result.schemaData,
+            apiProductSearch: result.apiProductSearch
         });
     }
     return next();
