de396c99be3a52c74ae0f9f97175eda042d7fcda
Ede Somogyi <126484858+ede-somogyi-algolia@users.noreply.github.com>
2023-03-16 16:59:08 +0200

    APEX-689: Migrated Algolia libraries to the cartridge (#14)
    * APEX-497: updated Node packages, cleaned up package.json, added linting rules and prettier
    * APEX-497: Refactored package.json, removed unnecessary packages, configured linter, updated Node package versions
    * APEX-689: migrated Algolia scripts to cartridge instead of loading them
    from a CDN service, misc. optimizations
    * APEX-689: updated CircleCI config, moved Algolia scripts to devDependencies
    * APEX-689: Committed Preact to the repository instead of including it remotely
    * APEX-689: relocated library files so that they can all be excluded from linting with a single rule
    * APEX-689: added Preact as an NPM devDependency
    * APEX-689: fixed remaining linting issues in the cartridge JS scripts
    * APEX-689: fixed indentation in autocomplete-config.js and pinned down the version for the htm package

--- cartridges/int_algolia_sfra/cartridge/static/default/js/algolia/autocomplete-config.js
+++ cartridges/int_algolia_sfra/cartridge/static/default/js/algolia/autocomplete-config.js
@@ -1,5 +1,9 @@
 /* global autocomplete, getAlgoliaResults, html, algoliaData  */
 
+/**
+ * Enables autocomplete
+ * @param {Object} config configuration object
+ */
 function enableAutocomplete(config) {
     const inputElements = document.querySelectorAll('#aa-search-input');
 
@@ -15,7 +19,7 @@ function enableAutocomplete(config) {
                 return [
                     {
                         sourceId: 'products',
-                        getItems({ query }) {
+                        getItems({ query }) { // eslint-disable-line no-shadow
                             return getAlgoliaResults({
                                 searchClient: config.searchClient,
                                 queries: [
@@ -51,21 +55,16 @@ function enableAutocomplete(config) {
                                     item.firstImage = smallImageGroup.images[0];
                                 }
                                 return html`
-                                  <div class="text-truncate text-nowrap">
-                                    <img class="swatch-circle hidden-xs-down"
-                                         src=${item.firstImage.dis_base_link}></img>
-                                    <a href=${item.url}>${components.Highlight({
-                                      hit: item,
-                                      attribute: "name",
-                                      tagName: "em"
-                                    })}</a>
-                                  </div>`;
+                                    <div class="text-truncate text-nowrap">
+                                        <img class="swatch-circle hidden-xs-down" src=${item.firstImage.dis_base_link}></img>
+                                        <a href=${item.url}>${components.Highlight({ hit: item, attribute: "name", tagName: "em" })}</a>
+                                    </div>`;
                             },
                         },
                     },
                     {
                         sourceId: 'categories',
-                        getItems({ query }) {
+                        getItems({ query }) { // eslint-disable-line no-shadow
                             return getAlgoliaResults({
                                 searchClient: config.searchClient,
                                 queries: [
@@ -96,10 +95,10 @@ function enableAutocomplete(config) {
                                   <div class="text-truncate text-nowrap">
                                     <img class="swatch-circle hidden-xs-down" src=${item.image}></img>
                                     <a href=${item.url}>${components.Highlight({
-                                      hit: item,
-                                      attribute: "name",
-                                      tagName: "em"
-                                    })}</a>
+    hit: item,
+    attribute: "name",
+    tagName: "em"
+})}</a>
                                   </div>`;
                             },
                         },
