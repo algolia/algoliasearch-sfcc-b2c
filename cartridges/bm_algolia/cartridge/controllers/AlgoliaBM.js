0a7ea531c648523f95ced8c72cdfc26ab4028394
Sylvain Bellone <sylvain.bellone@algolia.com>
2023-07-25 16:33:06 +0200

    refactor to add registration of destinations/tasks for categories

--- cartridges/bm_algolia/cartridge/controllers/AlgoliaBM.js
+++ cartridges/bm_algolia/cartridge/controllers/AlgoliaBM.js
@@ -52,7 +52,7 @@ function handleSettings() {
         try {
             updateIngestionConfig(appId, adminApiKey);
         } catch (e) {
-            Logger.error('Error when saving Algolia preferences: ' + e);
+            Logger.error('Error when updating Ingestion config: ' + e);
             algoliaData.setPreference('ApplicationID', formerAppId);
             algoliaData.setPreference('AdminApiKey', formerAdminApiKey);
             renderIndex(e.message);
@@ -80,12 +80,45 @@ function handleSettings() {
 /**
  * Registers or updates the Ingestion objects needed by the Ingestion API,
  * and save them as stringified JSON in the Indexing_Config custom parameter.
+ * The stored config has the following format:
+ * {
+ *   "sourceID": "f0b9058a-3618-4e70-be96-81000ae0190e",
+ *   "authenticationID": "318a9273-6068-4ef9-86bb-dba87800b72b",
+ *   "locales": {
+ *     "default": {
+ *       "products": {
+ *         "destinationID": "beccfdd9-8069-4a95-bcee-638341138bed",
+ *         "tasks": {
+ *           "replace": "7b023377-6ecf-4b3c-9a3b-b1f41bb20ec1"
+ *         }
+ *       },
+ *       "categories": {
+ *         "destinationID": "cd6729c3-608c-4683-826b-581584d12acf",
+ *         "tasks": {
+ *           "replace": "6a853b8f-cb4d-4d1f-bbfd-d2d1c3b65f5f"
+ *         }
+ *       }
+ *     },
+ *     "en_US": {
+ *       "products": {
+ *         "destinationID": "b1276044-05ea-4c9f-adb9-20937f412cc6",
+ *         "tasks": {
+ *           "replace": "39e91a77-0c03-45d5-a8c7-d5106d89e900"
+ *         }
+ *       },
+ *       "categories": {
+ *         // ...
+ *       }
+ *     }
+ *   }
+ * }
+ *
  * @param {string} appId - Algolia AppID
  * @param {string} adminApiKey - Algolia Admin API Key
  */
 function updateIngestionConfig(appId, adminApiKey) {
     var currentSite = algoliaData.getCurrentSite();
-    Logger.info("Registering indexing configuration... AppID=" + appId);
+    Logger.info("Updating indexing configuration... AppID=" + appId);
     var indexingConfig = JSON.parse(algoliaData.getPreference("Indexing_Config")) || {
         locales: {}
     };
@@ -95,6 +128,7 @@ function updateIngestionConfig(appId, adminApiKey) {
         sourceID = algoliaIngestionAPI.registerSource('Source of SFCC site ' + currentSite.getName(), currentSite.getID());
         Logger.info('Source registered. SourceID: ' + sourceID);
         indexingConfig.sourceID = sourceID;
+        algoliaData.setPreference('Indexing_Config', JSON.stringify(indexingConfig));
     } else {
         Logger.debug('Source already registered: ' + sourceID);
     }
@@ -109,6 +143,7 @@ function updateIngestionConfig(appId, adminApiKey) {
         });
         Logger.info('Authentication registered. AuthenticationID: ' + authenticationID);
         indexingConfig.authenticationID = authenticationID;
+        algoliaData.setPreference('Indexing_Config', JSON.stringify(indexingConfig));
     } else {
         Logger.info('Authentication already registered: ' + authenticationID + '. Updating...');
         algoliaIngestionAPI.updateAuthentication(authenticationID, {
@@ -118,48 +153,100 @@ function updateIngestionConfig(appId, adminApiKey) {
         });
     }
 
+    // The Ingestion API expects one destination per Algolia index.
+    // So we have to register two destinations for each locale: one for the products and one for the categories
     var siteLocales = currentSite.getAllowedLocales().toArray();
+    var indicesTypes = ['products', 'categories']
     for (var i = 0; i < siteLocales.length; ++i) {
-        if (!indexingConfig.locales) {
-            indexingConfig.locales = {};
-        }
         var locale = siteLocales[i];
-        var localeConfig = indexingConfig.locales[locale] || {};
-        var destinationID = localeConfig.destinationID;
-        if (!destinationID) {
-            Logger.info('Registering destination for locale: "' + locale + '"...');
-            destinationID = algoliaIngestionAPI.registerDestination({
-                name: currentSite.getID() + '_' + locale,
-                indexName: currentSite.getID() + '_products_' + locale,
-                authenticationID: authenticationID
-            });
-            Logger.info('Destination registered for locale "' + locale + '". DestinationID: ' + destinationID);
-            localeConfig.destinationID = destinationID;
-        } else {
-            Logger.info('Destination already exists for "' + locale + '": ' + destinationID);
-            algoliaIngestionAPI.updateDestination(destinationID, {
-                name: currentSite.getID() + '_' + locale,
-                indexName: currentSite.getID() + '_products_' + locale,
-                authenticationID: authenticationID
-            });
+        if (!indexingConfig.locales[locale]) {
+            indexingConfig.locales[locale] = {};
         }
 
-        var tasks = localeConfig.tasks || {};
-        if (!tasks.replace) {
-            Logger.info('Registering "replace" task for locale: "' + locale + '"...');
-            tasks.replace = algoliaIngestionAPI.registerTask({
+        for (var j = 0; j < indicesTypes.length; ++j) {
+            var type = indicesTypes[j];
+            if (!indexingConfig.locales[locale][type]) {
+                indexingConfig.locales[locale][type] = {};
+            }
+            var destinationID = registerOrUpdateDestination({
+                siteID: currentSite.getID(),
+                authenticationID: authenticationID,
+                locale: locale,
+                type: type,
+                existingDestinationID: indexingConfig.locales[locale][type].destinationID,
+            });
+            indexingConfig.locales[locale][type].destinationID = destinationID;
+            algoliaData.setPreference('Indexing_Config', JSON.stringify(indexingConfig));
+
+            var tasks = registerTasks({
                 sourceID: sourceID,
                 destinationID: destinationID,
-                action: 'replace'
+                locale: locale,
+                type: type,
+                existingTasks: indexingConfig.locales[locale][type].tasks,
             });
-            Logger.info('"replace" task created for locale "' + locale + '": ' + tasks.replace);
+            indexingConfig.locales[locale][type].tasks = tasks;
+            algoliaData.setPreference('Indexing_Config', JSON.stringify(indexingConfig));
         }
-        localeConfig.tasks = tasks;
-
-        indexingConfig.locales[locale] = localeConfig;
     }
+}
 
-    algoliaData.setPreference('Indexing_Config', JSON.stringify(indexingConfig));
+/**
+ * Registers or updates the Destination on the Ingestion API.
+ * @param {Object} params
+ * @param {string} params.siteID - SFCC Site ID
+ * @param {string} params.authenticationID - Algolia AuthenticationID
+ * @param {string} params.locale - Targeted locale
+ * @param {string} params.type - Type: 'products' or 'categories'
+ * @param {string?} params.existingDestinationID - The destinationID if it has already been registered
+ *
+ * @returns {string} destinationID
+ */
+function registerOrUpdateDestination({siteID, authenticationID, locale, type, existingDestinationID}) {
+    if (!existingDestinationID) {
+        Logger.info('Registering "' + type + '" destination for locale: "' + locale + '"...');
+        var destinationID = algoliaIngestionAPI.registerDestination({
+            name: siteID + '_' + type + '_' + locale,
+            indexName: algoliaData.getIndexPrefix() + '_' + type + '_' + locale,
+            authenticationID: authenticationID
+        });
+        Logger.info(`Destination "${type}" registered for locale "` + locale + '". DestinationID: ' + destinationID);
+        return destinationID;
+    } else {
+        Logger.info('Updating "' + type + '" destination for "' + locale + '": ' + locale + '" (' + existingDestinationID + ')...');
+        return algoliaIngestionAPI.updateDestination(existingDestinationID, {
+            name: siteID + '_' + type + '_' + locale,
+            indexName: algoliaData.getIndexPrefix() + '_' + type + '_' + locale,
+            authenticationID: authenticationID
+        });
+    }
+}
+
+/**
+ * Registers or updates the Destination on the Ingestion API.
+ * @param {Object} params
+ * @param {string} params.sourceID - Algolia sourceID
+ * @param {string} params.destinationID - Algolia DestinationID
+ * @param {string} params.locale - Targeted locale
+ * @param {string} params.type - Type: 'products' or 'categories'
+ * @param {object?} params.existingTasks - An object containing the registered task IDs
+ *
+ * @returns {Object} An object containing the registered task IDs
+ */
+function registerTasks({sourceID, destinationID, locale, type, existingTasks}) {
+    var tasks = existingTasks || {};
+    if (!tasks.replace) {
+        Logger.info('Registering "replace" task for "' + type + '" and locale: "' + locale + '"...');
+        tasks.replace = algoliaIngestionAPI.registerTask({
+            sourceID: sourceID,
+            destinationID: destinationID,
+            action: 'replace'
+        });
+        Logger.info('"replace" task created for "' + type + '" and locale "' + locale + '": ' + tasks.replace);
+    } else {
+        Logger.info('"replace" task already exists for "' + type + '" and locale "' + locale + '": ' + tasks.replace);
+    }
+    return tasks;
 }
 
 /**
