2d3ff4c9c438c0358af80356f1737f99ae1dcc73
Sylvain Bellone <sylvain.bellone@algolia.com>
2023-09-22 11:34:16 +0200

    feat: update sendProductUpdates job to direct indexing (#65)
    * feat: transform sendProductUpdates job use direct indexing
    * tests: add unit tests for the 'read' and 'process' methods

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,6 @@
+// Initialize some default mocks for all tests.
+
+// System classes
 jest.mock('dw/system/Logger', () => {
     return {
         info: jest.fn(),
@@ -11,3 +14,38 @@ jest.mock('dw/system/Logger', () => {
     }
 },
 {virtual: true});
+jest.mock('dw/system/Status', () => {}, {virtual: true});
+jest.mock('dw/system/Transaction', () => {}, {virtual: true});
+jest.mock('dw/system/System', () => {}, {virtual: true});
+jest.mock('dw/web/Resource', () => {
+    return {
+        msg: function() {}
+    }
+}, {virtual: true});
+
+// The following are not mocks, it points SFCC relative requires to the actual files
+// https://developer.salesforce.com/docs/commerce/b2c-commerce/guide/usingjavascriptmodules.html#path-lookup-behavior-of-the-require-method
+jest.mock('*/cartridge/scripts/algolia/customization/productModelCustomizer', () => {
+    return jest.requireActual('./cartridges/int_algolia/cartridge/scripts/algolia/customization/productModelCustomizer');
+}, {virtual: true});
+jest.mock('*/cartridge/scripts/algolia/filters/productFilter', () => {
+    return jest.requireActual('./cartridges/int_algolia/cartridge/scripts/algolia/filters/productFilter');
+}, {virtual: true});
+jest.mock('*/cartridge/scripts/algolia/helper/jobHelper', () => {
+    return jest.requireActual('./cartridges/int_algolia/cartridge/scripts/algolia/helper/jobHelper');
+}, {virtual: true});
+jest.mock('*/cartridge/scripts/algolia/helper/sendHelper', () => {
+    return jest.requireActual('./cartridges/int_algolia/cartridge/scripts/algolia/helper/sendHelper');
+}, {virtual: true});
+jest.mock('*/cartridge/scripts/algolia/lib/algoliaProductConfig', () => {
+    return jest.requireActual('./cartridges/int_algolia/cartridge/scripts/algolia/lib/algoliaProductConfig');
+}, {virtual: true});
+jest.mock('*/cartridge/scripts/algolia/lib/utils', () => {
+    return jest.requireActual('./cartridges/int_algolia/cartridge/scripts/algolia/lib/utils');
+}, {virtual: true});
+jest.mock('*/cartridge/scripts/algolia/lib/algoliaConstants', () => {
+    return jest.requireActual('./cartridges/int_algolia/cartridge/scripts/algolia/lib/algoliaConstants');
+}, {virtual: true});
+jest.mock('*/cartridge/scripts/algolia/model/algoliaLocalizedProduct', () => {
+    return jest.requireActual('./cartridges/int_algolia/cartridge/scripts/algolia/model/algoliaLocalizedProduct');
+}, {virtual: true});
