6fdb1302046b5db106ff7c1f476c2011c56f7482
Hayreddin Tuzel <hayre.tuzel@algolia.com>
2024-03-21 16:54:06 +0100

    Add getAnchorProductIds function to recommend/utils.js

--- cartridges/int_algolia/cartridge/scripts/algolia/recommend/utils.js
+++ cartridges/int_algolia/cartridge/scripts/algolia/recommend/utils.js
@@ -29,6 +29,34 @@ function getMasterProductIds(productIDs) {
     return masterProductIDs;
 }
 
+/**
+ * @description - Get anchor products for the given slot content
+ * @param {Object} slotcontent - Slot content
+ * @returns {string} - Product IDs
+ */
+function getAnchorProductIds(slotcontent) {
+    productIds = [];
+    var productIdsString = "";
+
+    if (session.privacy.algoliaAnchorProducts) {
+        var algoliaData = require('*/cartridge/scripts/algolia/lib/algoliaData');
+        var recordModel = algoliaData.getPreference('RecordModel');
+        if (recordModel === MASTER_LEVEL) {
+            productIds = getMasterProductIds(JSON.parse(session.privacy.algoliaAnchorProducts));
+        } else {
+            return session.privacy.algoliaAnchorProducts;
+        }
+    } else {
+        for (var i = 0; i < slotcontent.content.length; i++) {
+            var product = slotcontent.content[i];
+            productIds.push(product.ID);
+        }
+    }
+
+    return JSON.stringify(productIds);
+}
+
 module.exports = {
-    getMasterProductIds: getMasterProductIds
+    getMasterProductIds: getMasterProductIds,
+    getAnchorProductIds: getAnchorProductIds
 };
