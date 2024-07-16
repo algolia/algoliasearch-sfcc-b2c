61fe5ea9b112f52697b448b4e43ea4e86fcd8846
Hayreddin Tüzel <36648230+htuzel@users.noreply.github.com>
2024-07-16 11:11:52 +0200

    feat: Improved Caching

--- cartridges/int_algolia_sfra/cartridge/controllers/Search.js
+++ cartridges/int_algolia_sfra/cartridge/controllers/Search.js
@@ -48,6 +48,9 @@ server.replace('Show', cache.applyShortPromotionSensitiveCache, consentTracking.
             var hits;
             var contentHits;
             var queryHits;
+            // For algolia, we don't need personalized cache, as thereal  results fetched on the front-end
+            res.cachePeriod = 24; // eslint-disable-line no-param-reassign
+            res.cachePeriodUnit = 'hours'; // eslint-disable-line no-param-reassign
 
             // server-side rendering to improve SEO - makes a server-side request to Algolia to return CLP search results
             // only triggered when the user-agent looks like a bot, as we want it triggered only for search engines bots (DuckDuckBot, GoogleBot, BingBot, YandexBot, Baiduspider, ...)
