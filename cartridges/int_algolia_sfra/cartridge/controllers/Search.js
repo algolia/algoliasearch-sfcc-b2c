da18e1c85b4385c9c34cc4199cca0865c4c2de01
Hayreddin Tüzel <36648230+htuzel@users.noreply.github.com>
2024-07-17 17:41:48 +0200

    Cache Strategy Improved (#177)
    ## What is changed
    
    When we use Algolia, we do not need a personalized cache because the SSR
    result always will be same and we will fetch the results in the
    frontend. It will improve caching performance.

--- cartridges/int_algolia_sfra/cartridge/controllers/Search.js
+++ cartridges/int_algolia_sfra/cartridge/controllers/Search.js
@@ -48,6 +48,10 @@ server.replace('Show', cache.applyShortPromotionSensitiveCache, consentTracking.
             var hits;
             var contentHits;
             var queryHits;
+            //For Algolia, we don't need personalized cache, as the real results are fetched on the front-end.
+            res.cachePeriod = 24; // eslint-disable-line no-param-reassign
+            res.cachePeriodUnit = 'hours'; // eslint-disable-line no-param-reassign
+            res.personalized = true; // eslint-disable-line no-param-reassign
 
             // server-side rendering to improve SEO - makes a server-side request to Algolia to return CLP search results
             // only triggered when the user-agent looks like a bot, as we want it triggered only for search engines bots (DuckDuckBot, GoogleBot, BingBot, YandexBot, Baiduspider, ...)
