ac1156f2e6e6c84e73ef322df01f7bd40ebe8112
Hayreddin Tüzel <36648230+htuzel@users.noreply.github.com>
2024-07-26 17:12:32 +0200

    bugfix: Fixed Event issues for Base and Variant Record Models (#178)
    ## What is changed
    
    - Fixed Record model event issue (Logic moved to controllers)
       - Used master product id for base model record dynamically
    - Fixed Purchase event issue
    - order object had missing attributes and it is calculated on the
    backend and sent to frontend (`order.items.items` don't have a `price`
    property)
    - Fixed lastQuery logic (for `undefined` cases)
    - When we land to PDP, and there is no lastQuery, we were sending it as
    `undefined` and it was causing 422 error on Algolia side
    
    I also added some comments on the [Jira
    ticket](https://algolia.atlassian.net/browse/SFCC-367?atlOrigin=eyJpIjoiMGE0NzExZWUwMDVjNDY2MjliM2IxMTg3NmU3MTAxYTkiLCJwIjoiaiJ9),
    please let me know what do you think about it?

---
+++ cartridges/int_algolia_sfra/cartridge/controllers/Order.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var server = require('server');
+var base = module.superModule;
+var algoliaData = require('*/cartridge/scripts/algolia/lib/algoliaData');
+var priceFactory = require('*/cartridge/scripts/factories/price')
+
+var OrderMgr = require('dw/order/OrderMgr');
+
+server.extend(base);
+const MASTER_LEVEL = 'master-level';
+
+
+
+server.append('Confirm', function (req, res, next) {
+    if (algoliaData.getPreference('Enable') && algoliaData.getPreference('EnableInsights')) {
+
+        var viewData = res.getViewData();
+        var order = viewData.order;
+        var fullOrder = OrderMgr.getOrder(order.orderNumber);
+        var plis = fullOrder.getAllProductLineItems();
+        var algoliaProducts = [];
+        var currency;
+        var isBaseRecordModel = algoliaData.getPreference('RecordModel') === MASTER_LEVEL;
+
+        var pliArr = plis.toArray();
+
+        for (var i = 0; i < pliArr.length; i++) {
+            var product = pliArr[i].getProduct();
+
+            if (product) {
+                var algoliaProduct = {};
+
+                try {
+                    algoliaProduct.pid = algoliaData.getPreference('RecordModel') === MASTER_LEVEL ? product.masterProduct.ID : product.ID;
+                } catch (e) {
+                    algoliaProduct.pid = product.ID;
+                }
+
+                var price = priceFactory.getPrice(product);
+                algoliaProduct.price = price;
+                if (price.list) {
+                    algoliaProduct.discount = +(price.list.value - price.sales.value).toFixed(2);
+                }
+                currency = price.sales.currency;
+                algoliaProduct.qty = pliArr[i].quantityValue;
+                algoliaProducts.push(algoliaProduct);
+            }
+        };
+
+        const algoliaObj = {
+            items: algoliaProducts,
+            currency: currency
+        }
+
+        viewData.algoliaObj = algoliaObj;
+
+        res.setViewData(viewData);
+    }
+
+    next();
+});
+
+module.exports = server.exports();
