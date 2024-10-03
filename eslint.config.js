939d90d134d1696e2878934867a75d6cbaccd1f5
Sylvain Bellone <sylvain.bellone@algolia.com>
2024-10-03 16:07:04 +0200

    use single quotes

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,18 @@
-const globals = require("globals");
-const jquery = require("eslint-plugin-jquery");
-const jsdoc = require("eslint-plugin-jsdoc");
+const globals = require('globals');
+const jquery = require('eslint-plugin-jquery');
+const jsdoc = require('eslint-plugin-jsdoc');
 
 module.exports = [{
     ignores: [
-        "**/doc/",
-        "./*.js",
-        "**/jest.setup.js",
-        "**/bin/",
-        "**/workspace/",
-        "cartridges/modules",
-        "cartridges/algolia_sg_changes",
-        "cartridges/int_algolia_controllers/cartridge/static/default/js/lib",
-        "cartridges/int_algolia_sfra/cartridge/static/default/js/lib",
+        '**/doc/',
+        './*.js',
+        '**/jest.setup.js',
+        '**/bin/',
+        '**/workspace/',
+        'cartridges/modules',
+        'cartridges/algolia_sg_changes',
+        'cartridges/int_algolia_controllers/cartridge/static/default/js/lib',
+        'cartridges/int_algolia_sfra/cartridge/static/default/js/lib',
     ],
 }, {
     plugins: {
@@ -30,39 +30,39 @@ module.exports = [{
         },
 
         ecmaVersion: 2015,
-        sourceType: "script",
+        sourceType: 'script',
     },
 
     rules: {
-        "import/no-unresolved": "off",
+        'import/no-unresolved': 'off',
 
-        indent: ["error", 4, {
+        indent: ['error', 4, {
             SwitchCase: 1,
             VariableDeclarator: 1,
         }],
 
-        "func-names": "off",
-        "jsdoc/require-jsdoc": "error",
+        'func-names': 'off',
+        'jsdoc/require-jsdoc': 'error',
 
-        "vars-on-top": "off",
-        "global-require": "off",
+        'vars-on-top': 'off',
+        'global-require': 'off',
 
-        "no-shadow": ["error", {
-            allow: ["err", "callback"],
+        'no-shadow': ['error', {
+            allow: ['err', 'callback'],
         }],
 
-        "max-len": "off",
+        'max-len': 'off',
     }
 }, {
-    files: ["eslint.config.js", "test/**/*.js"],
+    files: ['eslint.config.js', 'test/**/*.js'],
     languageOptions: {
         globals: {
             ...globals.mocha,
         },
 
-        ecmaVersion: "latest",
+        ecmaVersion: 'latest',
     },
     rules: {
-        "jsdoc/require-jsdoc": "off"
+        'jsdoc/require-jsdoc': 'off'
     },
 }];
