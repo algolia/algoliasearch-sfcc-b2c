5f2f2d392061c211da9f637333dd39ea1404f50c
Hayreddin Tüzel <36648230+htuzel@users.noreply.github.com>
2024-10-23 12:16:17 +0200

    Cypress configuration

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-    projectId: "a9wvhs",
+  projectId: "a9wvhs",
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
 });
