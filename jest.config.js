de595522fe39c24b5ece19b40a1b9dd02f68bb23
Hayreddin Tüzel <36648230+htuzel@users.noreply.github.com>
2024-11-19 14:50:05 +0100

    Some improvements for testing

--- jest.config.js
+++ jest.config.js
@@ -9,8 +9,7 @@ const config = {
     clearMocks: true,
 
     // The paths to modules that run some code to configure or set up the testing environment before each test
-    setupFiles: ['<rootDir>/jest.setup.js'],
-    testPathIgnorePatterns: ['/node_modules/', '/test/e2e/'],
+    setupFiles: ['<rootDir>/jest.setup.js']
 };
 
 module.exports = config;
