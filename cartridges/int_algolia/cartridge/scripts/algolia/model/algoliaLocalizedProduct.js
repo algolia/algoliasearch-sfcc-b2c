c53949b2b3c16514d89ebf9f50678a584529b406
Hayreddin Tüzel <36648230+htuzel@users.noreply.github.com>
2025-01-07 17:18:38 +0100

    fix: improve product availability check in algoliaLocalizedProduct.js
    Updated the in_stock handler to utilize the ProductAvailabilityModel for better accuracy in determining product availability. The logic now checks the availability status directly for master products and variation groups, and retrieves the inventory record for other products to assess stock levels against the defined threshold.

--- cartridges/int_algolia/cartridge/scripts/algolia/model/algoliaLocalizedProduct.js
+++ cartridges/int_algolia/cartridge/scripts/algolia/model/algoliaLocalizedProduct.js
@@ -5,7 +5,7 @@ var Currency = require('dw/util/Currency');
 var PriceBookMgr = require('dw/catalog/PriceBookMgr');
 var PromotionMgr = require('dw/campaign/PromotionMgr');
 var URLUtils = require('dw/web/URLUtils');
-
+var ProductAvailabilityModel = require('dw/catalog/ProductAvailabilityModel');
 var modelHelper = require('*/cartridge/scripts/algolia/helper/modelHelper');
 var algoliaData = require('*/cartridge/scripts/algolia/lib/algoliaData');
 var algoliaProductConfig = require('*/cartridge/scripts/algolia/lib/algoliaProductConfig');
@@ -357,11 +357,15 @@ var aggregatedValueHandlers = {
         return pricebooks;
     },
     in_stock: function (product) {
+        let availabilityModel = product.getAvailabilityModel();
+
         if (product.isMaster() || product.isVariationGroup()) {
-            return undefined;
+            return availabilityModel.availabilityStatus === ProductAvailabilityModel.AVAILABILITY_STATUS_IN_STOCK;
         }
-        let inventoryRecord = product.getAvailabilityModel().getInventoryRecord(); // can be null
-        return (inventoryRecord ? inventoryRecord.getATS().getValue() >= ALGOLIA_IN_STOCK_THRESHOLD : false);
+
+        let inventoryRecord = availabilityModel.getInventoryRecord();
+        let inStock = (inventoryRecord ? inventoryRecord.getATS().getValue() >= ALGOLIA_IN_STOCK_THRESHOLD : false);
+        return inStock;
     },
     image_groups: function (product) {
         var imageGroupsArr = [];
