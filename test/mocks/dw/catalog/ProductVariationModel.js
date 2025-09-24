de6104a836a2180c094d5e6409cf96fb489b34f8
Sylvain Bellone <sylvain.bellone@algolia.com>
2025-09-24 18:05:47 +0200

    test: make ProductVariationModel.getAllValues() rely on the actual variants

--- test/mocks/dw/catalog/ProductVariationModel.js
+++ test/mocks/dw/catalog/ProductVariationModel.js
@@ -1,4 +1,5 @@
 const collectionHelper = require('../../helpers/collectionHelper');
+const ProductVariationAttributeValue = require('./ProductVariationAttributeValue');
 
 // https://salesforcecommercecloud.github.io/b2c-dev-doc/docs/current/scriptapi/html/api/class_dw_catalog_ProductVariationModel.html
 class ProductVariationModel {
@@ -6,6 +7,7 @@ class ProductVariationModel {
         this.productID = productID;
         this.master = master;
         this.images = images;
+        // Map of variationAttributeID => variationAttributeValueID, e.g. { color: 'JJB52A0', size: '004' }
         this.variationAttributes = variationAttributes;
         this.variants = variants;
     }
@@ -26,45 +28,25 @@ class ProductVariationModel {
         return productVariationAttributes[id][request.getLocale()];
     }
     getSelectedValue(attribute) {
-        const values = this.getAllValues(attribute);
-        return values.find(val => val.ID === this.variationAttributes[attribute.ID]);
+        return this.mockedProductVariationAttributeValuesById[this.variationAttributes[attribute.ID]];
     }
     setSelectedAttributeValue(variationAttributeID, variationAttributeValueID) {
         this.variationAttributes[variationAttributeID] = variationAttributeValueID;
     }
     getAllValues(variationAttribute) {
-        switch (variationAttribute.ID) {
-            case 'color':
-                switch (request.getLocale()) {
-                    case 'default':
-                    case 'en':
-                        return collectionHelper.createCollection([
-                            {
-                                ID: 'JJB52A0',
-                                value: 'JJB52A0',
-                                displayValue: 'Hot Pink Combo',
-                            },
-                        ]);
-                    case 'fr':
-                        return collectionHelper.createCollection([
-                            {
-                                ID: 'JJB52A0',
-                                value: 'JJB52A0',
-                                displayValue: 'Combo rose vif',
-                            },
-                        ]);
-                    default:
-                        return null;
-                }
-            case 'size':
-                return collectionHelper.createCollection([
-                    {
-                        ID: '004',
-                        value: '4',
-                        displayValue: '4',
-                    },
-                ]);
+        if (!this.variants) {
+            return collectionHelper.createCollection([])
         }
+        const uniqueValues = new Set();
+        const values = this.variants.reduce((acc, variant) => {
+            const attrValueID = variant.variationAttributes[variationAttribute.ID];
+            if (!uniqueValues.has(attrValueID)) {
+                uniqueValues.add(attrValueID);
+                acc.push(this.mockedProductVariationAttributeValuesById[attrValueID]);
+            }
+            return acc;
+        }, []);
+        return collectionHelper.createCollection(values);
     }
     getDefaultVariant() {
         return this.variants[0];
@@ -85,6 +67,42 @@ class ProductVariationModel {
                 return 'dwvar_' + this.productID + '_color';
         }
     }
+
+    // Mocks of all the ProductVariationAttributeValue used in the tests
+    mockedProductVariationAttributeValuesById = {
+        'JJB52A0': new ProductVariationAttributeValue({
+            ID: 'JJB52A0',
+            value: 'JJB52A0',
+            displayValues: {
+                en: 'Hot Pink Combo',
+                fr: 'Combo rose vif'
+            }
+        }),
+        'JJC05A0': new ProductVariationAttributeValue({
+            ID: 'JJC05A0',
+            value: 'JJC05A0',
+            displayValues: {
+                en: 'Navy',
+                fr: 'Marine'
+            }
+        }),
+        '004': new ProductVariationAttributeValue({
+            ID: '004',
+            value: '4',
+            displayValues: {
+                en: '4',
+                fr: '4'
+            }
+        }),
+        '006': new ProductVariationAttributeValue({
+            ID: '006',
+            value: '6',
+            displayValues: {
+                en: '6',
+                fr: '6'
+            }
+        }),
+    };
 }
 
 module.exports = ProductVariationModel;
