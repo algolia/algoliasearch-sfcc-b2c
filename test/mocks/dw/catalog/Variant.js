61d2ee3a7d42e28a0b5f268424a2ad5e20c1aec5
Ede Somogyi <ede.somogyi@algolia.com>
2025-12-22 23:31:44 +0200

    Fixed unit test

--- test/mocks/dw/catalog/Variant.js
+++ test/mocks/dw/catalog/Variant.js
@@ -27,6 +27,7 @@ class Variant extends MasterProduct {
                     }
                 }
             },
+            size: null,
             color: null,
             displaySize: '14cm',
             refinementSize: '4',
