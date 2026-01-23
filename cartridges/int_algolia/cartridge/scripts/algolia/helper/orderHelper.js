3187c700a2f5e34856b3bc6717f0676472df4522
Ede Somogyi <126484858+ede-somogyi-algolia@users.noreply.github.com>
2026-01-23 16:53:07 +0200

    fix: [SFCC-464] LocalesForIndexing is not being taken into account with real-time inventory updates (#252)
    Instead of updating only the indexes for the locales defined in
    `Algolia_LocalesForIndexing`, the indices for all site locales were
    updated whenever a product went out of stock due to customers placing
    orders.
    
    This is now fixed. Introduced new method `getActualLocalesForIndexing()`
    that returns the intersection of all available site locales and the
    locales defined in the `Algolia_LocalesForIndexing` site preferences --
    `AlgoliaOperations` are being generated only for these locales.
    
    This behavior is similar to how the jobs handle locales for the current
    job run, with the exception that in the case of jobs, there's an
    additional job-level parameter that can override the
    `Algolia_LocalesForIndexing` site preference on a per-job basis, which
    is not applicable here (since the order hook cannot have access to that
    job parameter).

--- cartridges/int_algolia/cartridge/scripts/algolia/helper/orderHelper.js
+++ cartridges/int_algolia/cartridge/scripts/algolia/helper/orderHelper.js
@@ -6,6 +6,30 @@ let AlgoliaLocalizedProduct = require('*/cartridge/scripts/algolia/model/algolia
 let algoliaData = require('*/cartridge/scripts/algolia/lib/algoliaData');
 
 /**
+ * Determines the locales to index for the current site by intersecting the site's allowed locales
+ * with the locales configured in Algolia under the `Algolia_LocalesForIndexing` site preference.
+ * @returns {string[]} Array of locales that are both allowed by the site and configured for indexing.
+ */
+function getActualLocalesForIndexing() {
+    let siteLocales = Site.getCurrent().getAllowedLocales().toArray();
+    let prefLocalesForIndexing = algoliaData.getSetOfArray('LocalesForIndexing');
+    let actualLocalesForIndexing = [];
+
+    if (empty(prefLocalesForIndexing)) {
+        actualLocalesForIndexing = siteLocales;
+    } else {
+        for (let i = 0; i < prefLocalesForIndexing.length; i++) {
+            let locale = prefLocalesForIndexing[i];
+            if (siteLocales.indexOf(locale) >= 0) {
+                actualLocalesForIndexing.push(locale);
+            }
+        }
+    }
+
+    return actualLocalesForIndexing;
+}
+
+/**
  * Generates an array of AlgoliaOperation objects to partially update product records in Algolia.
  * The function loops through allowed locales of the current site and creates a partialUpdateObject
  * operation for each locale.
@@ -14,28 +38,26 @@ let algoliaData = require('*/cartridge/scripts/algolia/lib/algoliaData');
  */
 function generateAlgoliaOperations(productConfig) {
     let algoliaOperations = [];
-    let siteLocales = Site.getCurrent().getAllowedLocales();
+    let actualLocalesToIndex = getActualLocalesForIndexing();
 
-    for (let i = 0; i < siteLocales.size(); i++) {
-        let locale = siteLocales[i];
+    for (let i = 0; i < actualLocalesToIndex.length; i++) {
+        let locale = actualLocalesToIndex[i];
         let indexName = algoliaData.calculateIndexName('products', locale);
 
         // Create localizedProductConfig - start with required properties
         let localizedProductConfig = productConfig;
         localizedProductConfig.locale = locale;
         let localizedProduct = new AlgoliaLocalizedProduct(localizedProductConfig);
-        algoliaOperations.push(
-            new jobHelper.AlgoliaOperation(
-                'partialUpdateObject',
-                localizedProduct,
-                indexName
-            )
-        );
-    }
+        algoliaOperations.push(new jobHelper.AlgoliaOperation(
+            'partialUpdateObject',
+            localizedProduct,
+            indexName
+        ));
+    };
 
     return algoliaOperations;
 }
 
 module.exports = {
-    generateAlgoliaOperations: generateAlgoliaOperations
+    generateAlgoliaOperations: generateAlgoliaOperations,
 };
